4718caadf3bfe70e84adf9951e8e393a85cac873
github username <github email address>
2019-10-14 14:02:19 -0400

    Done.

--- run.rb
+++ run.rb
@@ -4,6 +4,15 @@ require 'pry'
 
 
 #Test your code here
+z1 = Zoo.new("Bronx Zoo","Bronx, NY")
+z2= Zoo.new("San Diego Zoo","San Diego, CA")
+
+a1= Animal.new("lion",500,"Mufasa",z1)
+a2= Animal.new("zebra",300,"Chris Rock",z1)
+a3= Animal.new("lion",450,"Simba",z1)
+a4= Animal.new("tiger",600,"Terry",z2)
+a5= Animal.new("gorilla",400,"Greg",z2)
+
 
 
 binding.pry
